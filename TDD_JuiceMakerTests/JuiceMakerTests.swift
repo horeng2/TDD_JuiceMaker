fe06f6ce037b0744c0c165fcdfbcc32170deca35
horeng2 <huski.a.a@gmail.com>
2022-09-04 00:25:44 +0900

    feat: JuiceMakerTests 구현

--- TDD_JuiceMakerTests/JuiceMakerTests.swift
+++ TDD_JuiceMakerTests/JuiceMakerTests.swift
@@ -7,25 +7,77 @@
 
 import XCTest
 import RxSwift
+import RxBlocking
 @testable import TDD_JuiceMaker
 
-class JuiceMakerTests: XCTestCase {
-    var repository: Repository!
-    let diposeBag = DisposeBag()
-
+final class JuiceMakerTests: XCTestCase {
+    private var testJuice: Juice!
+    private var testFruitStock: [Fruit: Int]!
+    private var juiceMaker: JuiceMaker!
+    private var disposeBag = DisposeBag()
+    
     override func setUp() {
+        testFruitStock = [.strawberry: 10,
+                          .banana: 10,
+                          .pineapple: 10,
+                          .kiwi: 10,
+                          .mango: 10]
+        self.testJuice = .strawberryBananaJuice
+        
+        let repository = MockFruitRepository(data: testFruitStock)
+        self.juiceMaker = JuiceMaker(fruitRepository: repository)
     }
     
-    func test_makeJuice(_ juice: Juice) throws {
-    }
-    
-    func test_takeStock(for juice: Juice) throws {
-        juice.recipe.forEach{ fruit in
-            
+    func test_makeJuice() {
+        let observable = juiceMaker.makeJuice(testJuice)
+            .toBlocking()
+        let result = try! observable.single()
+        let expectation = testJuice.recipe.map{ requiredFruit, requiredCount in
+            requiredCount <= testFruitStock[requiredFruit]! ? true : false
         }
+            .contains(false) ? false : true
+        
+        XCTAssertEqual(result, expectation)
+        
+        
+        var decreaseResult = [Fruit: Int]()
+        testJuice.recipe.forEach{ requiredFruit, requiredCount in
+            juiceMaker.fetchFruitStock(requiredFruit)
+                .subscribe(onNext: { currentValue in
+                    decreaseResult.updateValue(currentValue, forKey: requiredFruit)
+                })
+                .disposed(by: disposeBag)
+        }
+        
+        var decreaseExpectation = [Fruit: Int]()
+        testJuice.recipe.forEach{ requiredFruit, requiredCount in
+            decreaseExpectation.updateValue(testFruitStock[requiredFruit]! - requiredCount, forKey: requiredFruit)
+        }
+        
+        XCTAssertEqual(expectation, expectation)
     }
     
-    func test_updateStock() {
+    func test_haveAllIngredients() {
+        let observable = juiceMaker.haveAllIngredients(for: testJuice)
+            .toBlocking()
+        let result = try! observable.single()
+        let expectation = testJuice.recipe.map{ requiredFruit, requiredCount in
+            requiredCount <= testFruitStock[requiredFruit]! ? true : false
+        }
+            .contains(false) ? false : true
 
+        XCTAssertEqual(result, expectation)
     }
+    
+    func test_haveFruitStock() {
+        let result = juiceMaker.haveFruitStock(for: testJuice)
+            .map{ observable in
+                try! observable.toBlocking().single()
+            }
+        let expectation = testJuice.recipe.map{ requiredFruit, requiredCount in
+            requiredCount <= testFruitStock[requiredFruit]! ? true : false
+        }
+        
+        XCTAssertEqual(result, expectation)
+  }
 }
