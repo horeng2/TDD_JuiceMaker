f7d35e114cd6dd3c3b9ec8d8ade84f125c2e2296
horeng2 <huski.a.a@gmail.com>
2022-09-04 05:28:41 +0900

    feat: Fruit 타입 내부프로퍼티 koreaName 구현

--- TDD_JuiceMaker/Entity/Juice.swift
+++ TDD_JuiceMaker/Entity/Juice.swift
@@ -27,6 +27,25 @@ enum Juice {
         }
     }
     
+    var koreaName: String {
+        switch self {
+        case .strawberryBananaJuice:
+            return "딸바주스"
+        case .mangoKiwiJuice:
+            return "망키주스"
+        case .strawberryJuice:
+            return "딸기주스"
+        case .bananaJuice:
+            return "바나나주스"
+        case .pineappleJuice:
+            return "파인애플주스"
+        case .kiwiJuice:
+            return "키위주스"
+        case .mangoJuice:
+            return "망고주스"
+        }
+    }
+    
     case strawberryBananaJuice
     case mangoKiwiJuice
     case strawberryJuice
