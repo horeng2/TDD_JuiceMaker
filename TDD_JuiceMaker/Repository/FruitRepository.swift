a517e47a7dde1d13a8e20c965e062770a14902f6
horeng2 <huski.a.a@gmail.com>
2022-09-03 19:56:03 +0900

    feat: FruitRepository 내부 decreaseStock 메소드 구현

--- TDD_JuiceMaker/Repository/FruitRepository.swift
+++ TDD_JuiceMaker/Repository/FruitRepository.swift
@@ -9,26 +9,34 @@ import Foundation
 import RxSwift
 
 class FruitRepository: Repository {
-    private var stock = [Fruit: Int]()
+    private var stockData = [Fruit: Int]()
     private let initialStock: Int
     
     init(initialStock: Int) {
         self.initialStock = initialStock
         
         Fruit.allCases.forEach { fruit in
-            stock.updateValue(self.initialStock, forKey: fruit)
+            stockData.updateValue(self.initialStock, forKey: fruit)
         }
     }
     
     func readStock(of fruit: Fruit) throws -> Observable<Int> {
-        guard let fruitStock = self.stock[fruit] else {
+        guard let fruitStock = self.stockData[fruit] else {
             throw ErrorType.readError
         }
         return Observable.just(fruitStock)
     }
     
     func updateStock(of fruit: Fruit, newValue: Int) {
-        self.stock.updateValue(newValue, forKey: fruit)
+        self.stockData.updateValue(newValue, forKey: fruit)
+    }
+    
+    func decreaseStock(of fruit: Fruit, by count: Int) {
+        guard let currentStock = self.stockData[fruit],
+              currentStock > .zero else {
+            return
+        }
+        self.stockData.updateValue(currentStock - count, forKey: fruit)
     }
 }
 
