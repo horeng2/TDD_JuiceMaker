f02380f30a03278f7d01fc2296c7af65ebc1a683
horeng2 <huski.a.a@gmail.com>
2022-09-03 17:22:15 +0900

    refactor: FruitRepositoryTest 구조 리팩토링

---
+++ TDD_JuiceMakerTests/FruitRepositoryTests.swift
@@ -0,0 +1,40 @@
+//
+//  TDD_JuiceMakerTests.swift
+//  TDD_JuiceMakerTests
+//
+//  Created by 서녕 on 2022/09/02.
+//
+
+import XCTest
+import RxSwift
+import RxBlocking
+@testable import TDD_JuiceMaker
+
+class FruitRepositoryTests: XCTestCase {
+    var testData = [Fruit: Int]()
+    var testFruit: Fruit!
+
+    override func setUp() {
+        Fruit.allCases.forEach { fruit in
+            self.testData.updateValue(10, forKey: fruit)
+        }
+        self.testFruit = .strawberry
+    }
+    
+    func test_readStock() {
+        let observable = Observable.just(testData[testFruit])
+            .toBlocking()
+
+        let result = try! observable.single()
+        let expectation = testData[testFruit]
+
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_updateStock() {
+        let newValue = 20
+        testData.updateValue(newValue, forKey: testFruit)
+        
+        XCTAssertEqual(newValue, testData[testFruit])
+    }
+}
